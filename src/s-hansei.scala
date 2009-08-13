c5f9eadfe33d6fa882627641c17b18af16909a6d
Paul Snively <psnively@mac.com>
2009-08-12 17:20:13 -0700

    Initial commit.

---
+++ src/s-hansei.scala
@@ -0,0 +1,52 @@
+import scala.continuations.ControlContext._
+
+sealed abstract class vc[+a]
+case class pV[+a](x: List[Pair[Double, vc[a]]]) extends vc[a]
+case class V[+a](x: a) extends vc[a]
+case class C[+a](x: unit => pV[a]) extends vc[a]
+
+object s_hansei
+{
+	def test1() =
+	{
+		reset
+		{
+			shift
+			{
+				k: (Int=>Int) => k(k(k(7)))
+			} + 1
+		} * 2
+	}
+
+	def foo() =
+	{
+		1 + shift{k: (Int=>Int) => k(k(k(7)))}
+	}
+
+	def bar() =
+	{
+		foo() * 2
+	}
+
+	def baz() =
+	{
+		reset(bar())
+	}
+
+	def pv_unit[a](x : a): pV[a] = pV(List((1.0, V(x))))
+
+	def dist[a](ch : List[Pair[Double, a]]) =
+	{
+		shift
+		{
+			k: (a => pV[a]) => 
+			ch map {(p: Double, v: a) => (p, C (() => k(v)))}
+		}
+	}
+
+	def main(args : Array[String]) =
+	{
+		println(test1())		// result 20
+		println(baz())			// result 70
+	}
+}
