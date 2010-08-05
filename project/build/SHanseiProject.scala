3726d5bd6f6daa18991d4a345065a1d9e88f127d
Paul Snively <psnively@mac.com>
2010-08-04 19:49:41 -0700

    Rebooting Scala HANSEI project.

--- project/build/SHanseiProject.scala
+++ project/build/SHanseiProject.scala
@@ -1,7 +1,11 @@
 import sbt._
 
-class SHanseiProject(info : ProjectInfo) extends DefaultProject(info)
+class SHanseiProject(info : ProjectInfo) extends DefaultProject(info) with AutoCompilerPlugins
 {
-  override def compileOptions = CompileOption("-Xplugin:lib/selectivecps-plugin.jar") :: super.compileOptions.toList
-  val specs = "org.scala-tools.testing" % "specs" % "1.6.0" % "test->default" from "http://specs.googlecode.com/svn/maven2/org/scala-tools/testing/specs/1.6.0-2.8.0/specs-1.6.0-2.8.0.jar"
+  val sxr = compilerPlugin("org.scala-tools.sxr" %% "sxr" % "0.2.6")
+  val continuations = compilerPlugin("org.scala-lang.plugins" % "continuations" % "2.8.0")
+  override def compileOptions = CompileOption("-P:continuations:enable") :: CompileOption("-P:sxr:base-directory:" + mainScalaSourcePath.asFile.getAbsolutePath) :: super.compileOptions.toList
+
+  val specs = "org.scala-tools.testing" %% "specs" % "1.6.5" % "test"
+  val scalacheck = "org.scala-tools.testing" %% "scalacheck" % "1.7" % "test"
 }
